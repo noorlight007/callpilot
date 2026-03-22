440506b261f31bc5709cc0f781692546b95f31df
noorlight07 <contactwnoor@gmail.com>
2026-03-22 23:18:20 +0600

    added sitemap

---
+++ src/app/robots.ts
@@ -0,0 +1,16 @@
+// app/robots.ts
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/api/",
+        "/admin/",
+      ],
+    },
+    sitemap: "https://callpilot.pro/sitemap.xml",
+  };
+}
