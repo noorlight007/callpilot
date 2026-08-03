34079010c3fd53f287ddea14492368c6de4f73c1
Md. Nazim Ahmed <77225770+nazimnk9@users.noreply.github.com>
2026-08-03 12:07:17 +0600

    add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
 import WhatsAppWidget from "@/components/WhatsAppWidget";
+import Script from "next/script";
 
 export const metadata: Metadata = {
     title: "CallPilot.pro",
@@ -16,10 +17,33 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-P3KNMXZJ"
+                        height="0"
+                        width="0"
+                        style={{ display: "none", visibility: "hidden" }}
+                    />
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
                 <Providers>
                     {children}
                     <WhatsAppWidget />
                 </Providers>
+                {/* Google Tag Manager */}
+                <Script
+                    id="gtm-script"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P3KNMXZJ');`,
+                    }}
+                />
+                {/* End Google Tag Manager */}
             </body>
         </html>
     );
